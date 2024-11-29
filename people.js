6b9a47d79b6bc5813dafa724ada3ca6e57b43886
Andy Simota <andyiulian.simota@gmail.com>
2024-11-29 16:24:55 +0100

    Add hobby function and update people.js to log hobbies

--- people.js
+++ people.js
@@ -1,3 +1,13 @@
 const importName = require("./utility/names");
 console.log(importName);
 console.log(importName.fullName("Gigi", "Becali"));
+
+const importHobby = require("./utility/hobbies");
+console.log(importHobby);
+console.log(
+  importHobby.hobby(
+    "F1",
+    "GP1",
+    "Qualsiasi altra cosa basta che non sia calcio"
+  )
+);
