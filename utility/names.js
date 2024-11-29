7ea2f00f6565196aad124e591ac7b34f60a20350
Andy Simota <andyiulian.simota@gmail.com>
2024-11-29 15:16:54 +0100

    Add package.json, hobbis.js and names.js with a function

---
+++ utility/names.js
@@ -0,0 +1,5 @@
+/* 1 - names.js
+1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+2. Esporta la funzione dal file. */
+const fullName = (firstName, lastName) => ({ firstName, lastName });
+export { fullName };
